ab3ec3b08564368dc5d4b2e956855d2de7512209
lucyoz <lucyoz.kwon@gmail.com>
2022-10-31 16:30:09 +0900

    Factory 추가

---
+++ practice/src/main/java/com/likelion/practice/parser/ParserFactory.java
@@ -0,0 +1,14 @@
+package com.likelion.practice.parser;
+
+import com.likelion.practice.domain.Hospital;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ParserFactory {
+
+    @Bean
+    public ReadLineContext<Hospital> hospitalReadLineContext(){
+        return new ReadLineContext<Hospital>(new HospitalParser());
+    }
+}
